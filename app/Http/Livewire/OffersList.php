1fdf755531c601e641b24122ac4c9264e924ee74
ArthurLeveque <arthur.lev44@gmail.com>
2021-11-24 19:56:26 +0100

    Ajout fonction suppression offre

--- app/Http/Livewire/OffersList.php
+++ app/Http/Livewire/OffersList.php
@@ -15,6 +15,41 @@ class OffersList extends Component
     // Valeur qui est rentrée dans le champ de recherche 
     public $search = '';
 
+    // Variable permettant d'afficher ou de cacher la modale de suppression d'offre
+    public $showDeleteModal = false;
+
+    // Valeurs permettant la suppression d'une offre 
+    public $offer_name = '';
+    public $offer_id = null;
+
+    // Ouverture de la modale de suppression tout en récupérant les données
+    public function showDeleteModal($offer_id)
+    {
+        $offer = Offer::find($offer_id);
+        $this->offer_id = $offer_id;
+        $this->offer_name = $offer->name;
+
+        $this->showDeleteModal = true;
+    }
+
+    // Fermeture de la modale de suppression tout en retirant les données stockées
+    public function hideDeleteModal()
+    {
+        $this->showDeleteModal = false;
+
+        $this->offer_id = '';
+        $this->offer_name = '';
+    }
+
+    // Suppression d'une offre, fermeture de la modale puis retrait des données stockées
+    public function deleteOffer() {
+        Offer::find($this->offer_id)->delete();
+
+        $this->showDeleteModal = false;
+
+        $this->offer_id = '';
+        $this->offer_name = '';
+    }
 
     // Récupération de toutes les offres correspondant au champ de recherche
     public function render()
