1f97a0338d8477ad3d2315d849703bed7cc46910
Haki44 <maxence_86@hotmail.fr>
2021-12-10 08:50:12 +0100

    Ajout des offres modif

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 flex flex-wrap justify-evenly">
             @foreach ($offers as $offer)
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-1/4 h-auto m-1">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-full sm:w-1/4 h-auto m-1">
                 <div class="p-6 bg-white border-b border-gray-200 h-full">
 
                         <div class="w-full h-full flex flex-col justify-between">
