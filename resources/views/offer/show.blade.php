f6763b4015e84f2aee941375e7a58fe2d863a704
Charly <charly2221@hotmail;fr>
2022-01-05 22:59:33 +0100

    Ajout du code Alpin pour open / close
    Ajout du code Alpin js pour gérer l'affichage / le masquage du textarea pour répondre.

--- resources/views/offer/show.blade.php
+++ resources/views/offer/show.blade.php
@@ -18,11 +18,11 @@
                 <p class="text-xl p-2">Description : {{ $offer->description }} Kips</p>
                 <p class="text-xl p-2">Prix : {{ $offer->price }} Kips</p>
                 <p class="text-xl p-2">Disponible le  : {{ date('d/m/Y', strtotime($offer->offer_day)) }} à {{ date('H:i', strtotime($offer->offer_day)) }}</p>
-                <div class="flex p-2 flex-col items-center">
+                <div class="flex p-2 flex-col items-center"  x-data="{ open:false }">
                     <a href="#" class="mr-2 mb-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Contacter {{ $offer->user->firstname }} pour + de précisions</a>
                     <a href="#" @click="open = !open" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center">Acheter ({{ $offer->price }} kips)</a>
 
-                    <div x-open="open" class="w-full sm:w-2/3">
+                    <dix x-show="open" class="w-full sm:w-2/3">
                         <x-auth-validation-errors class="mb-4" :errors="$errors" />
                         <form class="w-full sm:w-2/3" method="POST" action="{{ route('reply.store', ['offer' => $offer->id]) }}">
                         @csrf
@@ -33,11 +33,11 @@
                             </div>
                             <div class="flex items-center justify-end mt-4">
                                 <x-button class="ml-4">
-                                    Envoyer ma réponse
+                                    Envoyer ma réponse à {{ $offer->user->firstname }}
                                 </x-button>
                             </div>
                         </form>
-                    </div>
+                    </dix>
                 </div>
             </div>
 
