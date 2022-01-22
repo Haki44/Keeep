d5e96be0b9012ca98aaf44284249571f203edeae
Charly <charly2221@hotmail;fr>
2022-01-22 17:43:34 +0100

    Création d'un nouvel email pour les réponses

---
+++ app/Notifications/ReplyNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReplyNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($offer, $user_from)
+    {
+        $this->offer = $offer;
+        $this->user_from = $user_from;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())->markdown('emails.confirm-reply', ['offer' => $this->offer, 'user_from' => $this->user_from])
+                                  ->subject("Nouvelle réponse à l'offre : " . $this->offer->name);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
