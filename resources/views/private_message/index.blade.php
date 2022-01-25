6bd0d33e353af50fb74de6327b415d390939d69b
mailys-lq <mailys.lequintrec@gmail.com>
2022-01-25 20:29:16 +0100

    Ajout de l'heure sur chaque message

--- resources/views/private_message/index.blade.php
+++ resources/views/private_message/index.blade.php
@@ -26,7 +26,7 @@
                             <div class="chat-message">
                                 <div class="flex items-end justify-end">
                                     <div class="flex flex-col items-end order-1 max-w-xs mx-2 space-y-2 text-xs">
-                                        <div><span class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg rounded-br-none ">{{ $private_message->content }}</span></div>
+                                        <div class="flex flex-col"><span class="inline-block px-4 py-2 text-white bg-blue-600 rounded-lg rounded-br-none ">{{ $private_message->content }}</span><span>{{ date('H:i', strtotime($private_message->created_at))}}</span></div>
                                     </div>
                                     <img src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="order-2 w-6 h-6 rounded-full">
                                 </div>
@@ -35,7 +35,7 @@
                             <div class="chat-message">
                                 <div class="flex items-end">
                                     <div class="flex flex-col items-start order-2 max-w-xs mx-2 space-y-2 text-xs">
-                                        <div><span class="inline-block px-4 py-2 text-gray-600 bg-gray-300 rounded-lg rounded-bl-none">{{ $private_message->content }}</span></div>
+                                        <div class="flex flex-col"><span class="inline-block px-4 py-2 text-gray-600 bg-gray-300 rounded-lg rounded-bl-none">{{ $private_message->content }}</span><span>{{ date('H:i', strtotime($private_message->created_at))}}</span></div>
                                     </div>
                                     <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" class="order-1 w-6 h-6 rounded-full">
                                 </div>
