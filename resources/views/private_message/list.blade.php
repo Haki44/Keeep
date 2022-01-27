f2e7216236b6d3d2a548e158b684bcdfda7c729e
Maxence Bannier <70164079+Haki44@users.noreply.github.com>
2022-01-27 19:10:06 +0100

    delete dumb

--- resources/views/private_message/list.blade.php
+++ resources/views/private_message/list.blade.php
@@ -14,7 +14,6 @@
 
                     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 flex flex-wrap flex-col justify-evenly">
                         @foreach ($users as $user)
-                        {{-- @dump($user) --}}
                         {{-- <a href="{{route('private_message.index', $user->id)}}"> --}}
                             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-1/4 h-auto m-1">
                                 <div class="p-6 bg-white border-b border-gray-200 h-full">
