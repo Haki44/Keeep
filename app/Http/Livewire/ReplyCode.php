52d306dda196da33739bd755a1eaa29bf980dcb2
mailys-lq <mailys.lequintrec@gmail.com>
2022-02-07 00:46:51 +0100

    Update du code quand 3 tentative + modif affichage btn acheter sur l'offre

--- app/Http/Livewire/ReplyCode.php
+++ app/Http/Livewire/ReplyCode.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use App\Models\Reply;
 use Carbon\Carbon;
 use Livewire\Component;
+use App\Notifications\SendTradeEndCode;
+
 
 class ReplyCode extends Component
 {
@@ -40,12 +42,15 @@ class ReplyCode extends Component
 
         // dump(intval($this->reply->starting_code));
         // dd(intval($data['code']));
-        if($this->reply->started_at !== null){
+        if($this->reply->started_at === null){
             if(intval($data['code']) === intval($this->reply->starting_code)) {
                 Reply::where('id', $this->reply->id)->update([
                     'starting_code_count' => $this->reply->starting_code_count,
                     'started_at' => Carbon::now(),
                 ]);
+                
+                $reply = Reply::find($this->reply->id);
+                $reply->user->notify(new SendTradeEndCode($reply, auth()->user(), $this->reply->ending_code));
                 return redirect()->route('reply.show', $this->reply->id);
             } else {
                 // Increment le compteur
@@ -73,11 +78,17 @@ class ReplyCode extends Component
                 Reply::where('id', $this->reply->id)->update([
                     'ending_code_count' => $this->reply->ending_code_count,
                 ]);
+
+
                 // On vide le champs pour limiter les erreur de frappe et multisend
                 $this->code = '';
                 if($this->reply->ending_code_count == 3) {
-                        Reply::where('id', $this->reply->id)->delete();
-                return redirect()->route('reply.index');
+                    $ending_code = random_int(1000, 9999);
+                    Reply::where('id', $this->reply->id)->update([
+                        'ending_code' => $ending_code,
+                        'ending_code_count' => null
+                    ]);
+                    return redirect()->route('reply.show', $this->reply->id);
                 }
             }
         }
---
+++ app/Notifications/SendTradeEndCode.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Offer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class SendTradeCode extends Notification
+{
+    use Queueable;
+
+    public $offer;
+    public $reply;
+    public $user_from;
+    public $code;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($reply, $user_from, $code)
+    {
+        $this->reply = $reply;
+        $this->user_from = $user_from;
+        $this->code = $code;
+        $this->offer = Offer::where('id', $reply->offer_id)->first();
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject($this->reply->user->name . 'Voici votre code pour l\'offre Keeep ' . $this->offer->firstname)
+                    ->greeting('Bonjour ' . $this->reply->user->firstname . ',')
+                    ->line('Votre code est le ' . $this->code .', vous devrez le donner à ' . $this->offer->user->firstname . ' lors de la fin de la transaction')
+                    ->action('Voir la page de transaction', route('reply.show', $this->reply->id))
+                    ->line('A bientôt,')
+                    ->salutation('L\'équeeep');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
