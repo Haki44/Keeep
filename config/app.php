2ce561ebd62f15ebcd4c0e4895c719d8aad3ef6e
Charly Escalona <charly2221@hotmail.fr>
2022-02-19 22:27:34 +0100

    Enregistrement des helpers

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
+        App\Providers\TransactionServiceProvider::class,
 
     ],
 
@@ -229,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'TransactionsBalance' => App\Helpers\Transactions\Balance::class,
+        'TransactionHandling' => App\Helpers\Transactions\TransactionHandling::class
 
     ],
 
