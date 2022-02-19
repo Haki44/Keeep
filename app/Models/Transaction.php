b22b7adce76dc73d861b2ce0641e3eb5eb813277
Charly Escalona <charly2221@hotmail.fr>
2022-02-19 22:27:23 +0100

    Create Transaction.php

---
+++ app/Models/Transaction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'offer_id',
+        'reply_user_id',
+        'offer_user_id',
+        'amount',
+    ];
+}
