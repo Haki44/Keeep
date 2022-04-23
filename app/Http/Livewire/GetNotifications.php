9a1e4a5dcc983f6d974cbd27170ba8af584792e4
Charly Escalona <charly2221@hotmail.fr>
2022-04-23 23:36:16 +0200

    switch to readed state

--- app/Http/Livewire/GetNotifications.php
+++ app/Http/Livewire/GetNotifications.php
@@ -12,10 +12,23 @@ class GetNotifications extends Component
     public function render()
     {
         $private_messages = PrivateMessage::where('to_id', auth()->user()->id)->where('is_readed', NUll)->get();
+
         $replies = Reply::where('user_id', auth()->user()->id)->get();
+
         return view('livewire.get-notifications', [
-            'notifications' => $private_messages->merge($replies),
+            'replies' => $replies,
+            'privates_messages' => $private_messages,
+            'notification_count' => count($private_messages) + count($replies)
             ]);
 
+    }
+
+    public function switch_to_readed($id){
+
+        $private_message = PrivateMessage::find($id);
+
+        $private_message->is_readed = 1;
+
+        $private_message->save();
     }
 }
