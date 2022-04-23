247cc5c4b43e01b63ba9e5165676438eecac8cd6
Charly Escalona <charly2221@hotmail.fr>
2022-04-24 00:42:17 +0200

    Update get-notifications.blade.php

--- resources/views/livewire/get-notifications.blade.php
+++ resources/views/livewire/get-notifications.blade.php
@@ -8,7 +8,7 @@
             <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
         </svg>
     </button>
-    <div x-show="open" class="absolute h-64 top-20 w-64">
+    <div x-show="open" class="absolute h-64 top-20 -right-5 w-64">
         <ul class="flex w-[100%] bg-white flex-col h-auto">
             @foreach($replies as $reply)
 
